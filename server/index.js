1b7c1dd5334c6624188f2963431596fcf5cfea05
beyourbabe60 <beyourbabe60@gmail.com>
2023-03-13 23:40:06 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,12 +21,10 @@ console.clear();
 app.listen(PORT, () => console.log(`Serverr is listening on ${PORT}`));
 
 app.get("/", async (req, res) => {
-  const { page } = req.query;
+ 
   try {
-
     const objX = JSON.parse(datanook);
-    //console.log(objX)
-
+    console.log(objX)
     res?.status(200).json({
       NooK_Api: objX,
     });
@@ -35,3 +33,19 @@ app.get("/", async (req, res) => {
     res.status(500).send("Error");
   }
 });
+
+
+app.get("/filerows", async (req, res) => {
+    const { fr_id } = req.query;
+    try {
+      const objX = JSON.parse(datanook);
+      const fontPosts = objX.Datasets['Dataset'].filter((nook) => nook['FileRows'] == fr_id )
+      //console.log(fontPosts)
+      res?.status(200).json({
+        NooK_Api: fontPosts,
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Error");
+    }
+  });
