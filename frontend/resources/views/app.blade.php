a72315efb05b4cb42bcb217810d1bd5a9725b494
S.K <devprogram789@gmail.com>
2023-03-18 10:44:53 +0700

    up

--- frontend/resources/views/app.blade.php
+++ frontend/resources/views/app.blade.php
@@ -8,18 +8,10 @@
 </head>
 
 <body>
-    <div class="container mx-auto p-4">
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-        <div class="w-3/4">
-            <div class="whitespace-pre-wrap">Hey everyone!
-
-                It's almost 2022 and we still don't know if there is aliens living among us, or do we? Maybe the person
-                writing this is an alien.
-
-                You will never know.</div>
-        </div>
+    <div id="app">
+        <main>
+            @yield('content')
+        </main>
     </div>
 </body>
 
