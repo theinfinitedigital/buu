e613fcfc215aa127c1a24b57d750e9b190e35422
S.K <devprogram789@gmail.com>
2023-04-18 17:02:40 +0700

    up

--- frontend/resources/views/frontend/home.blade.php
+++ frontend/resources/views/frontend/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="grid grid-cols-1">
-        <img class="h-auto w-full object-cover" src="https://via.placeholder.com/1920x670" alt="People working on laptops" />
+        <img class="h-screen w-full object-cover" src="https://via.placeholder.com/1920x670" alt="People working on laptops" />
         <slidescomponent></slidescomponent>
     </div>
 @endsection
